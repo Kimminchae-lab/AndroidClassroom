a791a1902a7d4848a1b789fe90043ac2e8e86acd
Kimminchae <57434221+Kimminchae@users.noreply.github.com>
2020-04-23 12:29:53 +0900

    타이머 기능 강화

--- Chapter6_practice/boardgametimer/src/main/java/com/example/boardgametimer/MainActivity.java
+++ Chapter6_practice/boardgametimer/src/main/java/com/example/boardgametimer/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.boardgametimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Flushable;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnTimerSwitch, btnReset, btnPauseRestart;
     TextView textViewCopyright;
     CountDownTimer countDownTimer;
+    TextToSpeech textToSpeech;
 
     long fullTime, halfTime;
     boolean isPaused = false;       // pause 상태인가?
@@ -34,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
         btnReset = findViewById(R.id.btnReset);
         btnPauseRestart = findViewById(R.id.btnPauseRestart);
         textViewCopyright = findViewById(R.id.textViewCopyright);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
+
         //endregion
 
         //region btnTimerSwitch 클릭 처리
@@ -58,10 +76,27 @@ public class MainActivity extends AppCompatActivity {
         btnReset.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                resetTimer();   // 타이머 초기화
-                cancelTimer();  // 타이머 멈추기
+                // 잘못된 입력값 처리
+                if (editTextEnteredSeconds.getText().equals("")) {
+                    Toast.makeText(MainActivity.this, "입력값이 없습니다.", Toast.LENGTH_SHORT).show();
+                } else {
 
-                btnTimerSwitch.setEnabled(true);    // 버튼 기능 활성화
+                    // editText Focus 없애기, 키패드 숨기기
+                    if (editTextEnteredSeconds.hasFocus()) {
+                        editTextEnteredSeconds.clearFocus();
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(editTextEnteredSeconds.getWindowToken(), 0);
+                    }
+
+                    resetTimer();   // 타이머 초기화
+                    cancelTimer();  // 타이머 멈추기
+
+                    String msg = String.format("Reset to %d seconds", fullTime);
+                    textToSpeech.speak("Reset to 40 seconds", TextToSpeech.QUEUE_FLUSH, null);
+
+                    btnTimerSwitch.setEnabled(true);    // 버튼 기능 활성화
+
+                }
                 return true;
             }
         });
@@ -72,10 +107,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (isPaused) {
+                    textToSpeech.speak("restarted", TextToSpeech.QUEUE_FLUSH, null);
                     btnPauseRestart.setText(R.string.pause);
                     countDownTimer = countDownTimer(Long.parseLong(btnTimerSwitch.getText().toString()));
                     isPaused = false;
                 } else {
+                    textToSpeech.speak("paused", TextToSpeech.QUEUE_FLUSH, null);
                     countDownTimer.cancel();    // 타이머 멈추기
                     btnPauseRestart.setText(R.string.restart);
                     isPaused = true;
@@ -91,11 +128,21 @@ public class MainActivity extends AppCompatActivity {
             countDownTimer.cancel();
     }
 
+    // CountDownTimer 생성 및 반환
     private CountDownTimer countDownTimer(long t) {
         return new CountDownTimer(t * 1000 + 1000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 long currentTime = millisUntilFinished / 1000;
+                changeTextSize(currentTime);
+                if (currentTime == halfTime) {
+                    MediaPlayer.create(getApplicationContext(), R.raw.warning_sound).start();
+                }
+
+                if (currentTime <= 10) {
+                    btnTimerSwitch.setTextColor(Color.RED);
+                    textToSpeech.speak(String.valueOf(currentTime), TextToSpeech.QUEUE_FLUSH, null);
+                }
                 btnTimerSwitch.setText(String.valueOf(currentTime));
             }
 
@@ -114,9 +161,21 @@ public class MainActivity extends AppCompatActivity {
         fullTime = Long.parseLong(editTextEnteredSeconds.getText().toString());
         halfTime = Math.round(fullTime / 2);
 
+        changeTextSize(fullTime);
+
         btnTimerSwitch.setBackgroundColor(Color.YELLOW);
         btnTimerSwitch.setTextColor(Color.BLACK);
-
         btnTimerSwitch.setText(String.valueOf(fullTime));
+    }
+
+    // 숫자 크기에 따른 텍스트 크기 조정
+    private void changeTextSize(long seconds) {
+        if (seconds >= 100) {
+            btnTimerSwitch.setTextSize(200);
+        } else if (seconds >= 10) {
+            btnTimerSwitch.setTextSize(300);
+        } else {
+            btnTimerSwitch.setTextSize(400);
+        }
     }
 }
