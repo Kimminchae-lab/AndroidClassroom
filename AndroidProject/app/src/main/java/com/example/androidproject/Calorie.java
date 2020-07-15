38c0c8fabd7cc5a1f4ff5168a1141a4354ebf43c
Kimminchae <57434221+Kimminchae@users.noreply.github.com>
2020-07-16 08:36:45 +0900

    Revert "칼로리 계산, 목적지"
    This reverts commit afa95560add2265d839f26197e2ead3969c5135d.

--- AndroidProject/app/src/main/java/com/example/androidproject/Calorie.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/Calorie.java
@@ -2,43 +2,29 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
-public class Calorie extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class Calorie extends AppCompatActivity {
     TextView cal;
     TextView backToMap;
-    Double weight, distance;
-    Double kCal;
 
-    // region onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calorie);
-        setTitle("Calorie");
 
         cal = findViewById(R.id.cal);
         backToMap = findViewById(R.id.backToMap);
+        SQLiteDatabase calor = null;
+        calor = SQLiteDatabase.openOrCreateDatabase("cal.db", null);
 
-        Intent intent = getIntent();
-        weight = intent.getDoubleExtra("Weight", 1);
-        distance = intent.getDoubleExtra("distance", 1);
+        String sql = "CREATE TABLE CALORIE(CAL INTEGER)";
+        calor.execSQL(sql);
 
-        kCal = ((3.5 * 4 * weight * (distance / 6.0)) / 1000.0) * 5.0;
 
-        cal.setText(kCal.toString() + " Kcal");
-
-        backToMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
     }
-    // endregion
-
 }
